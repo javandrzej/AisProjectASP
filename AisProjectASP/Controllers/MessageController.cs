027023814865be7b7b386c8d11ab8da025c5de3c
Andrzej <xurten@gmail.com>
2017-01-19 16:39:28 +0100

    Added in MessageController actions for getting Messages. Creating a simple table with collecting data using ajax

--- AisProjectASP/Controllers/MessageController.cs
+++ AisProjectASP/Controllers/MessageController.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AisProjectASP.Utils;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AisProjectASP.Controllers
 {
     public class MessageController : Controller
     {
+        private ICacheHelper cacheHelper = new CacheHelper();
+
         // GET: Message
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: Message/GetMessages
+        public JsonResult GetMessages()
+        {
+            return Json(cacheHelper.GetMessages().OrderByDescending(i => i.Date).Take(10), JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Message/GetOlderMessages
+        public JsonResult GetOlderMessages()
+        {
+            return Json(cacheHelper.GetMessages().OrderByDescending(i => i.Date).Skip(10), JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Message/Details/id
+        public JsonResult Details(int id)
+        {
+            return Json(cacheHelper.GetMessages().Where(i => i.Id == id), JsonRequestBehavior.AllowGet);
         }
     }
 }
--- AisProjectASP/Global.asax.cs
+++ AisProjectASP/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AisProjectASP.Utils;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,6 +15,8 @@ namespace AisProjectASP
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ICacheHelper cacheHelper = new CacheHelper();
+            cacheHelper.CreateMessages();
         }
     }
 }
